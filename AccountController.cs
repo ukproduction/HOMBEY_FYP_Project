264f4f0d07e4dbc1af409c1fb76de4b976a90f5b
UK Production <55784647+ukproduction@users.noreply.github.com>
2020-08-24 23:44:25 +0500

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hombay.Models;
+
+
+namespace Hombay.Controllers { 
+
+    public class AccountController: Controller
+    {
+        
+        public ActionResult Home()
+        {
+            
+            return View("~/Views/Account/Home.cshtml");
+        }
+        public ActionResult SignUp()
+        {
+            return View("~/Views/Account/SignUp.cshtml");
+        }
+        public ActionResult Login()
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+        public ActionResult About()
+        {
+            return View("~/Views/Account/About.cshtml");
+        }
+        public ActionResult Gallery()
+        {
+            return View("~/Views/Account/Gallery.cshtml");
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HOMBEY_FYP_Project
+# Hombey_FYP_Project
+A construction platform is created using Asp.net MVC approach using html, css, javascript, Jquery, c#. 
